5e58e1bdc0b9f3337e004e12f4b528fca1cd7c0d
Zach Dickens <dickens.52@buckeyemail.osu.edu>
2019-03-08 15:01:13 -0500

    First Commit

---
+++ AlbumCollection.Tests/AlbumCollectionTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace AlbumCollection.Tests
+{
+    public class AlbumCollectionTest
+    {
+        [Fact]
+        public void Index_Sets_Model_To_All_Albums()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void Details_Sets_Model_To_Correct_Album()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ AlbumCollection/Migrations/20190308194143_InitialMigration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumCollection.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AlbumName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SongName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Albums");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
